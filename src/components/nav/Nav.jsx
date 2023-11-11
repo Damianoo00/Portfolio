99867899a461deddaf6d1ed42ebddb57f8780aed
Damianoo00 <dplaskowicki@simp-consulting.com>
2023-11-11 12:42:58 +0100

    nav module

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a className={activeNav === "#" ? "active" : ""} href="#">
+        <AiOutlineHome />
+      </a>
+      <a
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+        href="#about"
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+        href="#experience"
+      >
+        <BiBook />
+      </a>
+      <a
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+        href="#services"
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+        href="#contact"
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
